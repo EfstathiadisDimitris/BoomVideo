b95160555006421511cf55655e48711ed4b29945
Dimitris <d.efstathiadis92@gmail.com>
2018-12-23 21:42:09 +0200

    Added Folder Structure Phase-1

---
+++ src/Components/Main__App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Toggle, Portal, Modal } from '../Utilities';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Main__App">
+          <h1>It is Working</h1>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Utilities/index.js
@@ -0,0 +1,12 @@
+import Portal from './Portal';
+import Toggle from './Toggle';
+import Modal from './Modal';
+import Icon from './Icon';
+
+export {
+  Icon,
+  Modal,
+  Portal,
+  Toggle,
+};
+
